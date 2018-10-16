2d0630358b8bbf058ebd04c039e8303dd45edc9b
Rodrigo Hasse <rodrigohasse1@gmail.com>
2018-10-16 15:32:10 -0300

    commit de correcao

--- App1_Cell/App1_Cell/App1_Cell/Menu/Master.xaml.cs
+++ App1_Cell/App1_Cell/App1_Cell/Menu/Master.xaml.cs
@@ -20,31 +20,43 @@ namespace App1_Cell.Menu
         public void GoPagina1(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Paginas.TextCellPage());
+            IsPresented = false;
         }
 
         public void GoImageCellPage(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Paginas.ImageCellPage());
+            IsPresented = false;
         }
 
         public void GoEntryCellPage(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Paginas.EntryCellPage());
+            IsPresented = false;
         }
 
         public void GoSwitchCellPage(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Paginas.SwitchCellPage());
+            IsPresented = false;
         }
 
         public void GoViewCellPage(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Paginas.ViewCellPage());
+            IsPresented = false;
         }
 
         public void GoListViewPage(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Paginas.ListViewPage());
+            IsPresented = false;
+        }
+
+        public void GoListViewButtonPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.ListViewButtonPage());
+            IsPresented = false;
         }
     }
 }
