dcae3e2c516de81d8375bc88f20f4599fef2bdd2
Rodrigo Hasse <rodrigohasse1@gmail.com>
2018-10-16 16:53:38 -0300

    add projeto

---
+++ App_ListaBrasil/App_ListaBrasil/App_ListaBrasil/Municipios.xaml.cs
@@ -0,0 +1,31 @@
+﻿using App_ListaBrasil.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_ListaBrasil
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Municipios : ContentPage
+	{
+        private List<Municipio> ListaInternaMunicipio{ get; set; }
+        private List<Municipio> ListaFiltradaMunicipio { get; set; }
+		public Municipios (Estado estado)
+		{
+			InitializeComponent ();
+            ListaInternaMunicipio = Servico.Servico.GetMunicipios(estado.id);
+            ListaMunicipios.ItemsSource = ListaInternaMunicipio;
+		}
+
+        private void BuscaRapida(object sender, TextChangedEventArgs args)
+        {
+            ListaFiltradaMunicipio = ListaInternaMunicipio.Where(a => a.nome.Contains(args.NewTextValue)).ToList();
+            ListaMunicipios.ItemsSource = ListaFiltradaMunicipio;
+        }
+    }
+}
